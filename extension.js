71810a47da72cd5c7284d8c300947e6bfeddb333
he jiale <hjlarry@163.com>
2018-12-22 23:42:25 +0800

    add base translate features

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
+const request = require('request');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -17,11 +17,25 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('extension.translate', function () {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		let editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+		let document = editor.document;
+		let selection = editor.selection;
+		let text = document.getText(selection);
+		let url = 'https://translate.google.cn/translate_a/single?client=gtx&sl=auto&tl=zh-CN&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&kc=7&tk=514069.514069&q=' + text;
+		request(url, function (error, response, body) {
+			if (response && response.statusCode == 200) {
+				let result = JSON.parse(body);
+				vscode.window.showInformationMessage(result[0][0][0]);
+			} else {
+				console.log('error:', error);
+				vscode.window.showInformationMessage(error);
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
@@ -29,7 +43,7 @@ function activate(context) {
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
